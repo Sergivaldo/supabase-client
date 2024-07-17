b621ff4f49f1654316ba8e7ade0f1c62069027af
Sergivaldo <sergivaldo123@gmail.com>
2024-07-17 12:33:39 -0300

    feat: Serviço para fazer requisições para os endpoints de buckets do supabase storage.

---
+++ src/main/java/junior/sergivaldo/Bucket.java
@@ -0,0 +1,48 @@
+package junior.sergivaldo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Bucket {
+    private String id;
+    private String name;
+    @JsonProperty(value = "public")
+    private Boolean isPublic;
+    @JsonProperty(value = "file_size_limit", access = JsonProperty.Access.READ_ONLY)
+    private String fileSizeLimit;
+    @JsonProperty(value = "file_size_limits", access = JsonProperty.Access.WRITE_ONLY)
+    private List<String> fileSizeLimits;
+    @JsonProperty(value = "allowed_mime_types")
+    private List<String> allowedMimeTypes;
+    @JsonProperty(value = "created_at", access = JsonProperty.Access.READ_ONLY)
+    private String createdAt;
+    @JsonProperty(value = "updated_at", access = JsonProperty.Access.READ_ONLY)
+    private String updatedAt;
+    @JsonProperty( access = JsonProperty.Access.READ_ONLY)
+    private String owner;
+
+    @Override
+    public String toString() {
+        return "Bucket{" +
+                "  id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", isPublic=" + isPublic +
+                ", fileSizeLimit='" + fileSizeLimit + '\'' +
+                ", fileSizeLimits=" + fileSizeLimits +
+                ", allowedMimeTypes=" + allowedMimeTypes +
+                ", createdAt='" + createdAt + '\'' +
+                ", updatedAt='" + updatedAt + '\'' +
+                ", owner='" + owner + '\'' +
+                '}';
+    }
+
+}
---
+++ src/main/java/junior/sergivaldo/Main.java
@@ -0,0 +1,34 @@
+package junior.sergivaldo;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        final String AUTHORIZATION = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNldmlva3llY2VreGlsZWtmZ3B2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMDg3NDYyMCwiZXhwIjoyMDI2NDUwNjIwfQ.A8slrV0WWgyx4NhELThmXjn4lIUmgJUfcT4oZju7aYo";
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .addInterceptor(chain -> {
+                    var req = chain.request();
+                    var newReq = req.newBuilder()
+                            .addHeader("Authorization", AUTHORIZATION)
+                            .build();
+                    return chain.proceed(newReq);
+                })
+                .build();
+
+        Retrofit rf = new Retrofit.Builder()
+                .baseUrl("https://ceviokyecekxilekfgpv.supabase.co/storage/v1/")
+                .client(client)
+                .addConverterFactory(JacksonConverterFactory.create())
+                .build();
+
+        BucketService bs = rf.create(BucketService.class);
+        Optional<List<Bucket>> b = Optional.ofNullable(bs.getAll().execute().body());
+        b.ifPresent(buckets -> buckets.forEach(System.out::println));
+    }
+}
